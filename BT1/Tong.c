08dbfca6bd0ae4a0e628a35b40238ffcf1b2fe9c
trainv3 <nguyenvan.trai@hotmail.com>
2025-10-02 19:43:28 +0700

    T1

---
+++ BT1/Tong.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int a, b, Tong, Hieu;
+int main()
+{
+printf("Nhap so a="); 
+scanf("%d",&a);
+printf("Nhap so b=");
+scanf("%d",&b);
+ Tong=a+b;
+ Hieu=a-b;
+printf("Tong a+b =%d",Tong);
+printf("\n Hieu a-b=%d",Hieu);
+return 0;
+}
